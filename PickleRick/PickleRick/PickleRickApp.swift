4ce1d1764f525eda505efbcfd9d506e850b7650d
Pedro David Ramos Salamanca <pdramoss@gmail.com>
2023-02-01 12:12:47 -0500

    Create Project

---
+++ PickleRick/PickleRick/PickleRickApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickleRickApp.swift
+//  PickleRick
+//
+//  Created by Pedro Ramos on 1/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PickleRickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
